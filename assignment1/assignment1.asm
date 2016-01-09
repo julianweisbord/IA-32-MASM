76d0e924efee8c01b630948f2cf9bc39e4849a0f
julianweisbord <julianweisbord@gmail.com>
2016-01-08 17:43:05 -0800

    Created programmer introduction

--- assignment1/assignment1.asm
+++ assignment1/assignment1.asm
@@ -1,8 +1,10 @@
-TITLE Program Template     (template.asm)
+TITLE assignment 1     (assignment1.asm)
 
-; Author:
-; Course / Project ID                 Date:
-; Description:
+; Author: Julian Weisbord
+; Course / Project ID  CS271               Date:01/08/16
+; Description:  Displays name and program title. Prompts user for 2 numbers then
+; calculates the sum, difference, product, (integer) quotient and remainder of 
+; the numbers. Lastly, this program prints a terminating message.
 
 INCLUDE Irvine32.inc
 
@@ -10,12 +12,22 @@ INCLUDE Irvine32.inc
 
 .data
 
+my_name BYTE "Hello My Name Is Julian Weisbord",0 
+num1 DWORD ?
+num2 DWORD ?
+message BYTE "End of Program, goodbye...",0
+
 ; (insert variable definitions here)
+
 
 .code
 main PROC
 
-; (insert executable instructions here)
+;Introduction
+
+mov edx, OFFSET my_name
+call WriteString
+call CrLf
 
 	exit	; exit to operating system
 main ENDP
