f48fbdce5aff70a578351d2c8bfa86f39b4c1ccc
julianweisbord <julianweisbord@gmail.com>
2016-01-24 01:19:46 -0800

    start to calculate fibonacci sequence.
    Signed-off-by: julianweisbord <julianweisbord@gmail.com>

--- assignment2/assignment2.asm
+++ assignment2/assignment2.asm
@@ -24,7 +24,8 @@ goodbye BYTE "Goodbye, "
 numNum DWORd ?
 one DWORD 1
 fourtySix DWORD 46
-
+i DWORD 0
+j DWORD 5
 
 .code
 main PROC
@@ -51,7 +52,6 @@ call crlf
 
 ;Get num of fibonacci numbers
 
-
 ;if out of range, reprompt
 whileLoop:
 	mov edx, offset num_prompt
@@ -61,11 +61,15 @@ whileLoop:
 	jl throw_error
 	cmp eax, fourtySix
 	jg throw_error
-	;otherwise continue with program
+	mov ecx, j; for counted loop
+	;Display fibonacci numbers
+	whileFib:
+		forLoop:
+			
+			loop forLoop
 
 
-
-
+;farewell
 mov edx, offset goodbye
 call writestring
 mov edx, offset userName
